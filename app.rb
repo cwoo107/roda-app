7da2f58fadad5634dc0590e1fe5e188747305037
Caleb Woods <woodscaleb@gmail.com>
2019-09-28 18:38:50 -0700

    First commit

---
+++ app.rb
@@ -0,0 +1,19 @@
+require "roda"
+
+class Myapp < Roda
+  plugin :static, ["/images", "/css", "/js"]
+  plugin :render
+  plugin :head
+
+  route do |r|
+    r.root do
+        view("home")
+    end
+    r.get "about" do
+        view("about")
+    end
+    r.get "contact" do
+        view("contact")
+    end
+  end
+end
